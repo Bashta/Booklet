72f04c13aa0ec46318af5c603655016aefc6e845
Erison Veshi <erisonve@gmail.com>
2024-07-04 19:12:06 +0200

    Initial Commit

---
+++ Booklet/BookletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookletApp.swift
+//  Booklet
+//
+//  Created by Erison Veshi on 4.7.24.
+//
+
+import SwiftUI
+
+@main
+struct BookletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookletTests/BookletTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookletTests.swift
+//  BookletTests
+//
+//  Created by Erison Veshi on 4.7.24.
+//
+
+import Testing
+@testable import Booklet
+
+struct BookletTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
