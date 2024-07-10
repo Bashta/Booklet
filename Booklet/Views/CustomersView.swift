b551092300edb44951c77dd2e31ae3dc43b7af87
Erison Veshi <erisonve@gmail.com>
2024-07-10 20:05:30 +0200

    Cleanup CustomersView
    Minor code refactor

--- Booklet/Views/CustomersView.swift
+++ Booklet/Views/CustomersView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct CustomersView: View {
+    
+    // MARK: - State
+    
     @State private var customers: [Customer] = []
     @State private var selectedCustomer: Customer?
     @State private var isAddingNewCustomer = false
+    
+    // MARK: - Computed
     
     private var shouldShowEmptyStateView: Bool {
         customers.isEmpty && !isAddingNewCustomer
     }
     
     var body: some View {
+        content
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                newCustomerButton
+            }
+        }
+        .overlay {
+            if shouldShowEmptyStateView {
+                contentUnavailableView
+            }
+        }
+    }
+}
+
+// MARK: - Views
+
+private extension CustomersView {
+    var content: some View {
         Group {
             HSplitView {
                 // List of customers
@@ -45,39 +68,46 @@ struct CustomersView: View {
                 
             }
         }
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button(action: addCustomer) {
-                    Label("Add Customer", systemImage: "plus")
-                }
-            }
+    }
+    
+    var newCustomerButton: some View {
+        Button(action: addCustomer) {
+            Label("Add Customer", systemImage: "plus")
+                .foregroundStyle(Color.accentColor)
         }
-        .overlay {
-            if shouldShowEmptyStateView {
-                ZStack {
-                    Color(.black)
-                    ContentUnavailableView {
-                        Image(systemName: "person.3")
-                    } description: {
-                        Text("No customers yet")
-                    } actions: {
-                        Button {
-                            addCustomer()
-                        } label: {
-                            Text("Add Customer")
-                        }
-                    }
+    }
+    
+    var contentUnavailableView: some View {
+        ZStack {
+            Color(.black)
+            ContentUnavailableView {
+                Label("No customers yet", systemImage: "person.3")
+            } description: {
+                Text("Start by adding your first customer. Click the button below to get started.")
+            } actions: {
+                Button {
+                    addCustomer()
+                } label: {
+                    Image(systemName: "person.fill.badge.plus")
+                        .padding(8)
+                        .symbolEffect(.breathe)
+                        .foregroundStyle(Color.accentColor)
+                        .font(.title3)
                 }
             }
         }
     }
-    
-    private func addCustomer() {
+}
+
+// MARK: - Helpers
+
+private extension CustomersView {
+    func addCustomer() {
         isAddingNewCustomer = true
         selectedCustomer = nil
     }
     
-    private func binding(for customer: Customer) -> Binding<Customer> {
+    func binding(for customer: Customer) -> Binding<Customer> {
         Binding(
             get: { customer },
             set: { newValue in
