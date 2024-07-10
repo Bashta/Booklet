591e7650ccd4c68616f1157b4f3d727e7bebd5a9
severeduck <92829782+severeduck@users.noreply.github.com>
2024-07-10 20:12:49 +0200

    Update BookletTests.swift

--- BookletTests/BookletTests.swift
+++ BookletTests/BookletTests.swift
@@ -8,10 +8,4 @@
 import Testing
 @testable import Booklet
 
-struct BookletTests {
-
-    @Test func testExample() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+struct BookletTests { }
