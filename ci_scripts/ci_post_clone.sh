fe9f4fce61e71d0bfa0c4da9d246c8516e4b4618
Erison Veshi <erisonve@gmail.com>
2024-07-11 02:20:13 +0200

    Move the created plist to proper destination

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,24 +6,16 @@
 #  Created by Erison Veshi on 11.7.24.
 #
 
-# Debug: Print current working directory
-echo "Current working directory: $(pwd)"
-
-# Debug: Print contents of current directory
-echo "Contents of current directory:"
-ls -la
-
-# Debug: Print parent directory contents
-echo "Contents of parent directory:"
-ls -la ..
-
-# Attempt to write the file in the current directory
+# Create GoogleService-Info.plist in the current directory
 echo "$GOOGLE_SERVICE_INFO_PLIST" > "./GoogleService-Info.plist"
 
-# Debug: Confirm file creation
-echo "Contents of current directory after file creation:"
-ls -la
+# Move the file to the Booklet directory
+mv "./GoogleService-Info.plist" "../Booklet/"
 
-# Debug: Print first few lines of the created file
+# Confirm file movement
+echo "Contents of Booklet directory:"
+ls -la "../Booklet"
+
+# Print first few lines of the moved file
 echo "First few lines of GoogleService-Info.plist:"
-head -n 5 "./GoogleService-Info.plist"
+head -n 5 "../Booklet/GoogleService-Info.plist"
